479a146b35b18be102bc7365dac3098ec944cac6
cindych <cindych@sas.upenn.edu>
2021-05-04 00:51:21 -0400

    added function to list all data sets in a topic

--- src/DatasetParserMain.java
+++ src/DatasetParserMain.java
@@ -1,5 +1,8 @@
 public class DatasetParserMain {
     public static void main (String[] args) {
         
+        DatasetParser2 dsParser = new DatasetParser2(); 
+        dsParser.findTopics();
+        dsParser.listSetsInTopics("Health / Human Services");
     }
 }
